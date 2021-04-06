c55899e60c7ec6be328d5d0f21f0959e81a8f61d
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-04-06 20:51:41 -0300

    aula 05

---
+++ modulo04/aula05-reflect/reflect.js
@@ -0,0 +1,86 @@
+'use strict'
+const assert = require('assert')
+const myObj = {
+    add(myValue) {
+        return this.arg1 + this.arg2 + myValue
+    }
+}
+
+assert.deepStrictEqual(myObj.add.apply({ arg1: 10, arg2: 20 }, [100]), 130)
+
+// um problema que pode acontecer (raro)
+// Function.prototype.apply = () => { throw new TypeError('Eita!') }
+
+// esse aqui pode acontecer!
+myObj.add.apply = function () { throw new TypeError('Vixxx') }
+
+assert.throws(
+    () => myObj.add.apply({}, []),
+    {
+        name: 'TypeError',
+        message: 'Vixxx'
+    }
+)
+
+// usando reflect:
+const result = Reflect.apply(myObj.add, { arg1: 40, arg2: 20 }, [200])
+assert.deepStrictEqual(result, 260)
+// ---- apply
+
+// -- defineProperty
+
+// questões semânticas
+function MyDate() {}
+
+// feio pra Kct, tudo é Object, mas Object adicionando prop para uma function?
+Object.defineProperty(MyDate, 'withObject', { value: () => 'Hey there' })
+
+// agora faz mais sentido
+Reflect.defineProperty(MyDate, 'withReflection', { value: () => 'Hey dude' })
+
+assert.deepStrictEqual(MyDate.withObject(), 'Hey there')
+assert.deepStrictEqual(MyDate.withReflection(), 'Hey dude')
+
+// ---- deleteProperty
+const withDelete = { user: 'ErickWendel' }
+// imperformático, evitar ao máximo
+delete withDelete.user
+
+assert.deepStrictEqual(withDelete.hasOwnProperty('user'), false)
+
+const withReflection = { user: 'XuxaDaSilva' }
+Reflect.deleteProperty(withReflection, 'user')
+assert.deepStrictEqual(withReflection.hasOwnProperty('user'), false)
+
+// ---- get
+// Deveríamos fazer um get somente em instâncias de referências
+assert.deepStrictEqual(1['username'], undefined)
+
+// com reflection, uma exceção é lançada!
+assert.throws(() => Reflect.get(1, 'userName'), TypeError)
+// ---- get
+
+// ---- has
+assert.ok('superman' in { superman: '' })
+assert.ok(Reflect.has({ batman: '' }, 'batman'))
+// ---- has
+
+// --- ownKeys
+const user = Symbol('user')
+const databaseUser = {
+    id: 1,
+    [Symbol.for('password')]: 123,
+    [user]: 'erickwendel'
+}
+
+// Com os métodos do object, temos que fazer 2 requisições
+const objectKeys = [
+    ...Object.getOwnPropertyNames(databaseUser),
+    ...Object.getOwnPropertySymbols(databaseUser)
+]
+
+// console.log('objectKeys', objectKeys)
+assert.deepStrictEqual(objectKeys, ['id', Symbol.for('password'), user])
+
+// com reflection, só um método
+assert.deepStrictEqual(Reflect.ownKeys(databaseUser), objectKeys)
